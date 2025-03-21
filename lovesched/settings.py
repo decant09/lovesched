09f808876b8c0b0d1a9992f947ecf772783271b0
decant09 <decant09@hotmail.com>
2025-03-21 16:44:02 +0000

    Update for vscode, cloudinary & new neon db

--- lovesched/settings.py
+++ lovesched/settings.py
@@ -32,7 +32,7 @@ CLOUDINARY_STORAGE = {
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
